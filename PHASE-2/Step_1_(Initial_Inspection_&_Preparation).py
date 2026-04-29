62f92ef809ae0be33b9f219dd982948ad4f25e6c
Inakshi <inakshi.8734@gmail.com>
2026-04-29 22:45:17 +0530

    Delete PHASE-2/Step_1_(Initial_Inspection_&_Preparation).py

--- PHASE-2/Step_1_(Initial_Inspection_&_Preparation).py
+++
@@ -1,8 +0,0 @@
-print("\n ********************************** PHASE-2 (DATA CLEANING) **********************************\n")
-
-print("--- dataset ---\n")
-df_clean=df.copy()
-print(df_clean.head())
-df_clean.drop('name', axis=1, inplace=True, errors='ignore')
-print("--- null values count ---\n")
-print(df_clean.isnull().sum())
